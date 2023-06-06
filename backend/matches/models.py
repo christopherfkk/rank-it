4c08e5c793bd034df53792f45c3ae896ead23495
Christopher Fok <christopherfkk@gmail.com>
2023-06-06 17:06:03 +0800

    Add matches model

--- backend/matches/models.py
+++ backend/matches/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Match(models.Model):
+
+    class MatchType(models.TextChoices):
+        BADMINTON_SINGLES = "S", _("Singles")
+
+    submitter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    opponent = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    type = models.CharField(_("match type"), choices=MatchType.choices, default=MatchType.BADMINTON_SINGLES)
+    submitter_score = models.IntegerField()
+    opponent_score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.submitter.username} vs {self.opponent.username} on {self.created_at.date()}"
