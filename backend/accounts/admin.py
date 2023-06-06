4e2368da2513976ffed2b40a8c94a959745670b8
Christopher Fok <christopherfkk@gmail.com>
2023-06-06 13:05:51 +0800

    Add CustomUserCreationForm and CustomUserChangeForm for admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "dob",
+        "gender"
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name", "dob", "gender")}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name", "dob", "gender")}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
