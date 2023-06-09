cd0879b0d35702a2f405a42d13f1eb9b77bad706
Christopher Fok <christopherfkk@gmail.com>
2023-06-09 17:07:27 +0800

    Remove direct dependence on drf-jwt

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,8 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
     path('api/v1/', include('matches.urls')),
-    path('api/v1/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/auth/', include('dj_rest_auth.urls')),
 
 ]
