592abbcd3fda06f1e3754e42da685c08bc9af14b
Christopher Fok <christopherfkk@gmail.com>
2023-06-14 16:37:27 +0900

    Add permission class for only PUT GET own user instance

---
+++ backend/accounts/permission.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAdminUserOrSelf(BasePermission):
+    """
+    A permission class for the user model so that a superuser can perform any action
+    on all accounts or individual accounts. But a normal user can only PUT and GET
+    on their own accounts. However, a normal user can still GET all accounts.
+    """
+
+    def has_permission(self, request, view):
+        """Only allow PUT or GET requests for authenticated users"""
+        if request.method in ['PUT', 'GET'] and request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        """Allow users to modify their own instance"""
+        if request.method in ['PUT', 'GET'] and request.user == obj:
+            return True
+        return False
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,20 +1,22 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 from .serializers import AccountSerializer
+from .permission import IsAdminUserOrSelf
 
 
-class AccountViewSet(viewsets.ModelViewSet):  # new
-    permission_classes = (IsAdminUser, )
+class AccountViewSet(viewsets.ModelViewSet):
+    """View set for the user model"""
+    permission_classes = (IsAdminUserOrSelf, )
     queryset = get_user_model().objects.all()
     serializer_class = AccountSerializer
 
 
 class GoogleLogin(SocialLoginView):
+    """View for Google Login"""
     adapter_class = GoogleOAuth2Adapter
     callback_url = "http://127.0.0.1:8000/accounts/google/login/callback/"
     client_class = OAuth2Client
