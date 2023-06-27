98795bd67d1a2bf47664c057a065898eea5d2998
Christopher Fok <christopherfkk@gmail.com>
2023-06-27 21:54:34 +0900

    Add Community

--- backend/communities/models.py
+++ backend/communities/models.py
@@ -3,5 +3,9 @@ from django.db import models
 
 class Community(models.Model):
 
+    country = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+
     def __str__(self):
-        return
+        return f"Community {self.id} {self.name} in {self.city}, {self.country}"
