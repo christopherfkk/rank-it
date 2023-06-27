55f311e20060ba1d679c78a02a09bb836c2531a7
Christopher Fok <christopherfkk@gmail.com>
2023-06-27 20:54:12 +0900

    Add Match

--- backend/matches/models.py
+++ backend/matches/models.py
@@ -56,17 +56,13 @@ class MatchOffer(models.Model):
         blank=True,
         on_delete=models.CASCADE(),
     )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-    )
-    update_at = models.DateTimeField(
-        auto_now=True,
-    )
+    created_at = models.DateTimeField(auto_now_add=True,)
+    update_at = models.DateTimeField(auto_now=True,)
 
     def __str__(self):
         return \
             f"""
-            MatchOffer {self.id} for {self.type}
+            MatchOffer {self.id} for {self.type} with status {self.status}
             submitted by {self.submitter.username} to {self.opponent.username}
             on {self.start_datetime}
             """
@@ -74,17 +70,62 @@ class MatchOffer(models.Model):
 
 class Match(models.Model):
 
+    class MatchStatus(models.TextChoices):
+        PENDING = "P", _("Pending")
+        CANCELLED = "C", _("Cancelled")
+        AWAITING_CONFIRMATION = "A", _("Awaiting confirmation")
+        PLAYED = "P", _("Played")
 
+    class MatchCancelledReasonPhrase(models.TextChoices):
+        SUBMITTER_CANCELLED = "SC", _("The submitter cancelled the match")
+        OPPONENT_CANCELLED = "OC", _("The opponent cancelled the match")
+        NO_POST_MATCH_FEEDBACK = "NPMF", _("Neither submitter/opponent submitted post match feedback")
+        CONFLICTED_REPORTED_SCORES = "CRS", _("Reported scores in post match feedback don't match after resubmission")
 
-    submitter = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='match_submitter', on_delete=models.CASCADE)
-    opponent = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='match_opponent', on_delete=models.CASCADE)
-    submitter_score = models.IntegerField()
-    opponent_score = models.IntegerField()
+    match_offer = models.ForeignKey(
+        MatchOffer,
+        on_delete=models.CASCADE(),
+    )
+    submitter = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='match_submitter',
+        on_delete=models.CASCADE,
+    )
+    opponent = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='match_opponent',
+        on_delete=models.CASCADE,
+    )
+    submitter_score = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+    opponent_score = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+    status = models.CharField(
+        _("match status"),
+        choices=MatchStatus.choices,
+        max_length=20,
+        default=MatchStatus.PENDING,
+    )
+    cancelled_reason_phrase = models.CharField(
+        _("match cancelled reason phrase"),
+        choices=MatchCancelledReasonPhrase.choices,
+        max_length=20,
+        null=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.submitter.username} vs {self.opponent.username} ({self.submitter_score}-{self.opponent_score}) on {self.created_at.date()}"
+        return \
+            f"""
+            Match {self.id} with status {self.status} between
+            submitter {self.submitter.username} and opponent {self.opponent.username}
+            on {self.match_offer.start_datetime}
+            """
 
 
 class PostMatchFeedback(models.Model):
