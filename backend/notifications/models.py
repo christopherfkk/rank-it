1dc1aaf52c3d7ecd09ae62b4f5305cda0650bf83
Christopher Fok <christopherfkk@gmail.com>
2023-07-01 20:25:14 +0900

    Defined notif models

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class NotificationType(models.Model):
+
+    def __str__(self):
+        return
+
+
+class NotificationTrigger(models.Model):
+
+    def __str__(self):
+        return
+
+
+class Notification(models.Model):
+
+    def __str__(self):
+        return
