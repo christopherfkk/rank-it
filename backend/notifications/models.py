1569e743c80830304cfe60aba005d4f9f0dafb12
Christopher Fok <christopherfkk@gmail.com>
2023-07-01 22:12:33 +0900

    Add notification type fixtures

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -4,20 +4,61 @@ from django.conf import settings
 
 
 class NotificationType(models.Model):
-    """Static table preloaded from fixtures"""
+    """Static table preloaded from fixtures under fixtures/notification_type.yaml"""
 
     class Entity(models.TextChoices):
-        MATCH_OFFER = "Match Offer", _("Match Offer")
+        MATCH_OFFER = "MatchOffer", _("Match Offer")
         MATCH = "Match", _("Match")
-        POST_MATCH_FEEDBACK = "Post-match Feedback", _("Post-match Feedback")
+        POST_MATCH_FEEDBACK = "PostMatchFeedback", _("Post-match Feedback")
         COMMUNITY = "Community", _("Community")
         ACCOUNT = "Account", _("Account")
+
+    class Description(models.TextChoices):
+        MATCH_OFFER_SENT = "Sent a match offer"
+        MATCH_OFFER_ACCEPTED = "Accepted a match offer"
+        MATCH_OFFER_DECLINED = "Declined a match offer"
+        MATCH_OFFER_RESCINDED = "Rescinded a match offer"
+        MATCH_CREATED = "Created a match"
+        MATCH_CANCELLED = "Cancelled a match"
+        POST_MATCH_FEEDBACK_REPORTED = "Reported a post-match feedback"
 
     entity = models.CharField(
         choices=Entity.choices,
         max_length=20
     )
-    description = models.TextField()
+    description = models.CharField(
+        choices=Description.choices,
+        max_length=100
+    )
+
+    def __str__(self):
+        return f"<NotificationType> for entity {self.entity}: {self.description}"
+
+    def get_message_template(self, **kwargs):
+
+        try:
+            if self.entity == NotificationType.Entity.MATCH_OFFER:
+                if self.description == NotificationType.Description.MATCH_OFFER_SENT:
+                    return f"{kwargs['actor']} sent you a match offer!"
+                if self.description == NotificationType.Description.MATCH_OFFER_ACCEPTED:
+                    return f"{kwargs['actor']} accepted your match offer!"
+                if self.description == NotificationType.Description.MATCH_OFFER_DECLINED:
+                    return f"{kwargs['actor']} declined your match offer."
+                if self.description == NotificationType.Description.MATCH_OFFER_RESCINDED:
+                    return f"{kwargs['actor']} rescinded their match offer."
+
+            if self.entity == NotificationType.Entity.MATCH:
+                if self.description == NotificationType.Description.MATCH_CREATED:
+                    return f"Your match {kwargs['submitter']} vs {kwargs['opponent']} is scheduled!"
+                if self.description == NotificationType.Description.MATCH_CANCELLED:
+                    return f"Your match {kwargs['submitter']} vs {kwargs['opponent']} is cancelled"
+
+            if self.entity == NotificationType.Entity.POST_MATCH_FEEDBACK:
+                if self.description == NotificationType.Description.POST_MATCH_FEEDBACK_REPORTED:
+                    return f"Your opponent {kwargs['reporter']} just submitted a post-match feedback to you!"
+
+        except Exception:
+            raise ValueError("Invalid NotificationType: wrongly defined entity or kwargs")
 
 
 class NotificationObject(models.Model):
@@ -30,7 +71,7 @@ class NotificationObject(models.Model):
     created_at = models.DateTimeField(auto_now_add=True,)
 
     def __str__(self):
-        return f"Notification Object for entity {self.notification_type.entity}: {self.notification_type.description}"
+        return f"<Notification Object> for entity {self.notification_type.entity}: {self.notification_type.description}"
 
 
 class NotificationTrigger(models.Model):
@@ -47,7 +88,7 @@ class NotificationTrigger(models.Model):
     def __str__(self):
         return \
             f"""
-            Notification triggered by {self.actor.username}
+            <NotificationTrigger> by {self.actor.username}
             regarding {self.notification_object}
             """
 
@@ -74,7 +115,7 @@ class Notification(models.Model):
     def __str__(self):
         return \
             f"""
-            Notification for {self.notifier.username}
+            <Notification> for {self.notifier.username}
             regarding {self.notification_object}
             with status {self.status}
             """
