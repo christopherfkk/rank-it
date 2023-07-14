18f34ef7927a2334b03108c5f2ebf9460d53aeb7
hugo <hugo@spchu-MAC.local>
2023-07-14 16:08:21 +0900

    reset password flow

--- frontend/screens/ResetPassword.tsx
+++ frontend/screens/ResetPassword.tsx
@@ -18,28 +18,78 @@ import { Color, FontFamily, Border, FontSize, Padding, Auth } from "../GlobalSty
 import BASE_URL from '../apiConfig';
 
 const ResetPassword = () => {
+
+  const [email, setEmail] = useState("")
+  const [error,setError] = useState("")
+  
+  const handleReset = () => {
+    // Perform your API call or network request here to send email and password to the backend
+    // You can use libraries like Axios or the built-in fetch function
+  
+    // Example using fetch:
+      const resetData = {
+        email: email,
+      };
+      // Perform your API call or network request here to send email and password to the backend
+      fetch(`${BASE_URL}/accounts/password/reset/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(resetData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Handle the response from the backend
+          console.log(data);
+    
+          // Check if the login was successful (adjust this based on your backend response)
+          const registerSuccess = data.access !== undefined;
+  
+          if (registerSuccess) {
+            navigation.navigate("Login");
+          }
+          else {
+            setError(Object.values(data))
+          }
+        })
+        .catch((error) => {
+          console.error(error)
+          setError(error)
+        });
+    };
+
+
   return (
     <SafeAreaView style={[Auth.background]}>
       <View style={Auth.body}>
       <Text style={Auth.heading1}>
       Reset Password
       </Text>
-        <Text style={Auth.heading2}>
+      <Text style={Auth.heading2}>
       Enter your email to reset your password
       </Text>
-        <TouchableOpacity
-          style={[Auth.button]}
-          activeOpacity={0.2}
-          onPress={handleReset}
-        >
-          <TouchableOpacity activeOpacity={0.2} onPress={() => {}}>
-            <Text style={[Auth.buttonText]}>Sign-up</Text>
-          </TouchableOpacity>
-        </TouchableOpacity>
+      <TextInput
+            style={[Auth.textInputBoxStyle]}
+            placeholder="Enter your email "
+            keyboardType="email-address"
+            autoCapitalize="none"
+            placeholderTextColor="#737373"
+            onChangeText={(text: string) => setEmail(text)}
+            contextMenuHidden // Disable context menu (copy-paste actions)
+          />
+      <TouchableOpacity
+        style={[Auth.button]}
+        activeOpacity={0.2}
+        onPress = {handleReset}
+      >
+        <Text style={[Auth.buttonText]}>Reset</Text>
+      </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
+
 
 const styles = StyleSheet.create({
   signUpBodyFlexBox: {
