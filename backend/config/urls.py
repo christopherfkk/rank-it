f07cfe1153d88440006c7be379c9f05cc5777bdb
Your Name <hugosiuyh@gmail.comn>
2023-07-19 16:32:38 +0900

    rever

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -32,3 +32,5 @@ urlpatterns = [
     path('api/schema', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
