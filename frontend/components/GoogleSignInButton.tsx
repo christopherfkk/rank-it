7f06ce177c19fa5273f09a111c774bea770c0366
hugo <hugosiuyh@gmail.com>
2023-07-19 15:31:00 +0900

    asyncstorage and context work!

--- frontend/components/GoogleSignInButton.tsx
+++ frontend/components/GoogleSignInButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { TouchableOpacity, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import * as WebBrowser from 'expo-web-browser';
@@ -10,11 +10,38 @@ import { useRegContext, ACTIONS } from '../RegContext';
 
 WebBrowser.maybeCompleteAuthSession();
 
+const fetchUserInfo = async (accessToken: string) => {
+  try {
+    const googleResponse = await fetch(`${apiConfig.BASE_URL}/accounts/google/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ 'access_token': accessToken }),
+    });
+    
+    if (!googleResponse.ok) {
+      // Handle the error response here if the API request was not successful
+      console.error('Error: API request not successful');
+      // Throw an error to trigger the catch block
+      throw new Error('API request failed');
+    }
+
+    const data = await googleResponse.json();
+    return data;
+  } catch (error) {
+    // Handle any errors that occurred during the API call
+    console.error("Error fetching user info:", error);
+    // You can also re-throw the error here if needed.
+    // throw error;
+  }
+};
 
 const storeUserInfo = async (data, dispatch) => {
   try {
-    await AsyncStorage.setItem('accessToken', JSON.stringify(data.access));
-    await AsyncStorage.setItem('refreshToken', JSON.stringify(data.refresh));
+    console.log(data.access);
+    AsyncStorage.setItem('accessToken', JSON.stringify(data.access));
+    AsyncStorage.setItem('refreshToken', JSON.stringify(data.refresh));
 
     dispatch({ type: ACTIONS.SET_PROFILE_PHOTO, payload: data.user.avatar });
     dispatch({ type: ACTIONS.SET_BLURB, payload: data.blurb });
@@ -22,6 +49,7 @@ const storeUserInfo = async (data, dispatch) => {
     dispatch({ type: ACTIONS.SET_LAST_NAME, payload: data.user.last_name });
     dispatch({ type: ACTIONS.SET_GENDER, payload: data.user.gender });
     dispatch({ type: ACTIONS.SET_PHONE_NUMBER, payload: data.user.phone_number });
+    console.log('hi3');
   } catch (error) {
     console.error("Error storing user info in AsyncStorage:", error);
   }
@@ -29,9 +57,9 @@ const storeUserInfo = async (data, dispatch) => {
 
 const GoogleSignInButton = () => {
   const navigation = useNavigation();
+  const [accessToken, setAccessToken] = useState("");
+  const { state, dispatch } = useRegContext();
   const { clientId, iosClientId, androidClientId } = apiConfig.google;
-  const [accessToken, setAccessToken] =  useState("")
-  const { dispatch } = useRegContext();
   const [request, response, promptAsync] = Google.useAuthRequest({
     clientId: clientId,
     iosClientId: iosClientId,
@@ -39,42 +67,38 @@ const GoogleSignInButton = () => {
   });
 
   useEffect(() => {
-    if (response?.type === "success" && response.authentication) {
-      setAccessToken(response.authentication.accessToken)
-      fetchUserInfo()
-    }
-  }, [response,accessToken]);
+    checkcontext(dispatch);
+  }, [dispatch]);
 
-  async function fetchUserInfo() {
+
+  const handleGoogleSignIn = async () => {
     try {
-      const response = await fetch(`${apiConfig.BASE_URL}/accounts/google/`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({'access_token': accessToken}),
-      });
-      if (!response.ok) {
-        // Handle the error response here if the API request was not successful
-        console.error('Error: API request not successful');
-        // Throw an error to trigger the catch block
-        throw new Error('API request failed');
+      const result = await promptAsync();
+      if (result?.type === "success" && result.authentication) {
+        setAccessToken(result.authentication.accessToken);
+        const data = await fetchUserInfo(result.authentication.accessToken);
+        console.log(data)
+        await storeUserInfo(data, dispatch);
       }
-  
-      const data = await response.json();
-
-      storeUserInfo(data,dispatch)
-      // Return the data so it can be used outside the function if needed
-      return data;
     } catch (error) {
-      // Handle any errors that occurred during the API call
-      console.error("Error fetching user info:", error);
-      // You can also re-throw the error here if needed.
-      // throw error;
-    }}
+      console.error("Error during Google sign-in:", error);
+    }
+  };
+
+
+  const checkcontext = (dispatch) => {
+    // Check if 'firstName' value exists in the state object.
+    if (state.firstName !== null) {
+      // Do something if the 'firstName' value exists in the state.
+      console.log('firstName:', state.profilePhoto);
+    } else {
+      // Do something else if the 'firstName' value doesn't exist in the state.
+      console.log('firstName not set.');
+    }
+  }
 
   return (
-    <TouchableOpacity style={[Auth.google, Auth.googleFlexBox]} activeOpacity={0.2} onPress={() => promptAsync()}>
+    <TouchableOpacity style={[Auth.google, Auth.googleFlexBox]} activeOpacity={0.2} onPress={handleGoogleSignIn}>
       <Image style={Auth.logogoogle} source={require("../assets/group-18.png")} />
       <Text style={[Auth.buttonText, { color: Color.black, paddingLeft: 10 }]} numberOfLines={1}>
         Sign in with Google
