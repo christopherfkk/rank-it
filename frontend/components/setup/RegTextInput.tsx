b3e462c074130dfb59ae085059a4fbd1c6c53ef9
Yau Hin Siu <93142129+hugosiuyh@users.noreply.github.com>
2023-07-24 14:46:02 +0900

    Create RegTextInput.tsx

---
+++ frontend/components/setup/RegTextInput.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { TextInput } from "react-native";
+import { Reg } from "../GlobalStyles";
+
+const RegTextInput = ({ placeholder, value, onChangeText }) => {
+  return (
+    <TextInput
+      style={[Reg.textboxText, Reg.textboxShadowBox]}
+      placeholder={placeholder}
+      placeholderTextColor="#737373" // Set placeholder text color here
+      value={value}
+      onChangeText={onChangeText}
+    />
+  );
+};
+
+export default RegTextInput;
