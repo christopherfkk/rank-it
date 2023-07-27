d4919f66c58d150f43fc7e0f0e3bb9c5c4f5c7e4
Christopher Fok <christopherfkk@gmail.com>
2023-07-27 16:49:18 +0900

    Change

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,7 +1,7 @@
 # build_files.sh
-pip install pipenv && pipenv install && pipenv shell
+pipenv install && pipenv shell
 
 # make migrations
-python3.9 manage.py migrate
-python3.9 manage.py loaddata prod.yaml
-python3.9 manage.py collectstatic
+python manage.py migrate
+python manage.py loaddata prod.yaml
+python manage.py collectstatic
