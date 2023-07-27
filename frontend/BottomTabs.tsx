6fcad2a7ee7d6b214b3e74e0233b3f4cc52e52ac
hugosiuyh <93142129+hugosiuyh@users.noreply.github.com>
2023-07-27 13:21:10 +0900

    more

--- frontend/BottomTabs.tsx
+++ frontend/BottomTabs.tsx
@@ -2,13 +2,9 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React, {useState} from 'react';
 import RankingNav from './components/nav/RankingNav';
 import MatchesNav from './components/nav/MatchesNav';
-import ChatNav from './components/nav/ChatNav';
 import ProfileNav from './components/nav/ProfileNav';
-import {Pressable, View} from 'react-native';
 import Ranking from './screens/home/Ranking';
 import Profile from './screens/home/Profile';
-import ModalPostmatchfeedback from './components/home/ModalPostmatchfeedback';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Tab = createBottomTabNavigator();
 
@@ -33,7 +29,7 @@ function BottomTabs({navigation}: any) {
             />
             <Tab.Screen
                 name="Match"
-                component={ModalPostmatchfeedback}
+                component={Ranking}
                 options={{
                     headerShown: false,
                     tabBarIcon: MatchesNav.type,
