22cdd7ce2f689c392c11186cc0cc4c54733889c8
hugosiuyh <93142129+hugosiuyh@users.noreply.github.com>
2023-07-31 17:19:15 +0900

    stretch image

--- frontend/GlobalStyles.ts
+++ frontend/GlobalStyles.ts
@@ -329,52 +329,7 @@ export const Home = {
         alignSelf: "center", // INSTEAD OF STRETCH
         width: "95%", // ADD THIS
         maxWidth: 500, // ADD THIS
+        // borderWidth: 1,
     },
 }
 
-export const Nav = {
-    nav: {
-        flex: 1,
-        flexDirection: "column",
-        alignSelf: "stretch",
-        alignItems: "center",
-        justifyContent: "center",
-        overflow: "visible",
-    },
-    icon: {
-        width: 20,
-        height: 20,
-        overflow: "visible",
-    },
-    iconImage: {
-        width: 20,
-        height: 20,
-        borderRadius: 0,
-    },
-    text: {
-        fontSize: FontSize.size_3xs,
-        letterSpacing: 1,
-        fontFamily: FontFamily.bebasNeueRegular,
-        color: Color.lightLabelPrimary,
-        textAlign: "center",
-    },
-}
-
-export const ProfileStyles = {
-    button: {
-        flexDirection: "row",
-        gap: 5,
-        backgroundColor: Color.whitesmoke_300,
-        borderRadius: Border.br_131xl,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "auto",
-        paddingVertical: "1%",
-        paddingHorizontal: "5%",
-        marginVertical: "2%"
-    },
-    buttonText: {
-        fontFamily: FontFamily.manropeMedium,
-        color: Color.gray_100,
-    },
-}
