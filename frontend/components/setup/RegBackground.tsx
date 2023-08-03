39d93a7671497242f7d5c40ccc1797fabfbecddd
Christopher Fok <christopherfkk@gmail.com>
2023-08-03 15:27:21 +0900

    Show icons

--- frontend/components/setup/RegBackground.tsx
+++ frontend/components/setup/RegBackground.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { ImageBackground } from "react-native";
-import { Reg } from "../../GlobalStyles";
+import {ImageBackground, View} from "react-native";
+import {Reg} from "../../GlobalStyles";
 
-const RegBackground = ({ children }) => {
-  return (
-    <ImageBackground
-      style={Reg.signUpBody}
-      resizeMode="cover"
-      source={require("../../assets/badminton-bg.png")}
-    >
-      {children}
-    </ImageBackground>
-  );
+const RegBackground = ({children}) => {
+    return (
+        <ImageBackground
+            imageStyle={{resizeMode: "cover"}}
+            style={Reg.signUpBody}
+            source={require("../../assets/badminton-bg.png")}
+        >
+            {children}
+        </ImageBackground>
+    );
 };
 
 export default RegBackground;
