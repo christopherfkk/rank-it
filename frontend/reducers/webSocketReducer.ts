e707fb6d653c1f2577e34ae6669d8dc38efe31ac
Yau Hin Siu <93142129+hugosiuyh@users.noreply.github.com>
2023-08-04 16:28:23 +0900

    Update webSocketReducer.ts

--- frontend/reducers/webSocketReducer.ts
+++ frontend/reducers/webSocketReducer.ts
@@ -1,14 +1,14 @@
 interface WebSocketState {
     socket_ranks: WebSocket | null;
     socket_notifs: WebSocket | null;
-    isPopupOpen: boolean;
+    // isPopupOpen: boolean;
 }
 
 export enum WebSocketActionTypes {
     INIT_RANKS = 'INIT_RANKS',
     INIT_NOTIFS = 'INIT_NOTIFS',
-    OPEN_POPUP = 'OPEN_POPUP',
-    CLOSE_POPUP = 'CLOSE_POPUP',
+    // OPEN_POPUP = 'OPEN_POPUP',
+    // CLOSE_POPUP = 'CLOSE_POPUP',
 }
 
 interface InitRanksAction {
@@ -21,15 +21,15 @@ interface InitNotifsAction {
     payload: WebSocket;
 }
 
-interface OpenPopupAction {
-    type: WebSocketActionTypes.OPEN_POPUP;
-}
+// interface OpenPopupAction {
+//     type: WebSocketActionTypes.OPEN_POPUP;
+// }
 
-interface ClosePopupAction {
-    type: WebSocketActionTypes.CLOSE_POPUP;
+// interface ClosePopupAction {
+//     type: WebSocketActionTypes.CLOSE_POPUP;
 }
-// ... define more Action
-type webSocketAction = InitRanksAction | InitNotifsAction | OpenPopupAction | ClosePopupAction // | ... more Action
+// ... define more Action OpenPopupAction | ClosePopupAction
+type webSocketAction = InitRanksAction | InitNotifsAction // | ... more Action
 
 const webSocketReducer = (state: WebSocketState = { socket_ranks: null, socket_notifs: null, isPopupOpen: false }, action: webSocketAction): WebSocketState => {
     switch (action.type) {
@@ -37,10 +37,10 @@ const webSocketReducer = (state: WebSocketState = { socket_ranks: null, socket_n
             return { ...state, socket_ranks: action.payload };
         case WebSocketActionTypes.INIT_NOTIFS:
             return { ...state, socket_notifs: action.payload };
-        case WebSocketActionTypes.OPEN_POPUP:
-            return { ...state, isPopupOpen: true };
-        case WebSocketActionTypes.CLOSE_POPUP:
-            return { ...state, isPopupOpen: false };
+        // case WebSocketActionTypes.OPEN_POPUP:
+        //     return { ...state, isPopupOpen: true };
+        // case WebSocketActionTypes.CLOSE_POPUP:
+        //     return { ...state, isPopupOpen: false };
         // ... add more cases depending on actions
         default:
             return state;
