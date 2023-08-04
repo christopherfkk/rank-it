57c21f2511fb68f8e96c106ad6874974c684f695
hugosiuyh <93142129+hugosiuyh@users.noreply.github.com>
2023-08-04 13:26:02 +0900

    create modal

---
+++ frontend/components/home/ModalReminder.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Modal
+} from "react-native";
+import { Color, FontFamily } from "../../GlobalStyles";
+import BackButton from "./BackButton";
+import RegButton from "../setup/RegButton"
+import { useNavigation } from '@react-navigation/native';
+
+type ModalPostmatchfeedbackType = {
+  visible: boolean; // Add the 'visible' property to the type
+  onClose?: () => void;
+  nMatchesUnconfirmed: number;
+};
+
+const ModalPostmatchfeedbackA = ({ visible, onClose, nMatchesUnconfirmed}: ModalPostmatchfeedbackType) => {
+  const navigation = useNavigation();
+
+  return (
+    <Modal animationType="slide" transparent visible={visible}>
+      <SafeAreaView style={styles.modalPostmatchfeedback}>
+          <BackButton onPress={() => onClose()} />
+          <View style={styles.heading1box}>
+            <Text style={styles.heading1}>Confirmation Required</Text>
+            <Text style={styles.heading1}> Hey, there are still {nMatchesUnconfirmed} matches awaiting your confirmation. Please complete the post-match feedback to keep our rankings precise. Thanks!</Text>
+          </View>
+          <View style={styles.spacing} />
+          <RegButton  
+            navigation={navigation} 
+            screenName='MatchConfirm' 
+            buttonText='Confirm Matches'/>  
+      </SafeAreaView>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  modalPostmatchfeedback: {
+    backgroundColor: Color.white,
+    alignItems: "center", // Center items horizontally
+    justifyContent: "center", // Center items vertically
+    flex: 1,
+    paddingBottom: 30,
+  },
+  errorText: {
+    color: "red",
+    fontFamily: FontFamily.manropeRegular,
+    fontSize: 10,
+    textAlign: "center",
+  },
+  heading1box: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 100,
+  },
+  heading1: {
+    fontSize: 40,
+    letterSpacing: 1.2,
+    fontFamily: FontFamily.bebasNeueRegular,
+  },
+  spacing: {
+    height: 20, // Set the desired vertical spacing between components
+  },
+});
+export default ModalPostmatchfeedbackA;
--- frontend/reducers/webSocketReducer.ts
+++ frontend/reducers/webSocketReducer.ts
@@ -1,11 +1,14 @@
 interface WebSocketState {
     socket_ranks: WebSocket | null;
     socket_notifs: WebSocket | null;
+    isPopupOpen: boolean;
 }
 
 export enum WebSocketActionTypes {
     INIT_RANKS = 'INIT_RANKS',
     INIT_NOTIFS = 'INIT_NOTIFS',
+    OPEN_POPUP = 'OPEN_POPUP',
+    CLOSE_POPUP = 'CLOSE_POPUP',
 }
 
 interface InitRanksAction {
@@ -18,20 +21,30 @@ interface InitNotifsAction {
     payload: WebSocket;
 }
 
+interface OpenPopupAction {
+    type: WebSocketActionTypes.OPEN_POPUP;
+}
+
+interface ClosePopupAction {
+    type: WebSocketActionTypes.CLOSE_POPUP;
+}
 // ... define more Action
+type webSocketAction = InitRanksAction | InitNotifsAction | OpenPopupAction | ClosePopupAction // | ... more Action
 
-type webSocketAction = InitRanksAction | InitNotifsAction // | ... more Action
-
-const webSockerReducer = (state: WebSocketState = { socket_ranks: null, socket_notifs: null }, action: webSocketAction): WebSocketState => {
+const webSocketReducer = (state: WebSocketState = { socket_ranks: null, socket_notifs: null, isPopupOpen: false }, action: webSocketAction): WebSocketState => {
     switch (action.type) {
         case WebSocketActionTypes.INIT_RANKS:
             return { ...state, socket_ranks: action.payload };
         case WebSocketActionTypes.INIT_NOTIFS:
             return { ...state, socket_notifs: action.payload };
+        case WebSocketActionTypes.OPEN_POPUP:
+            return { ...state, isPopupOpen: true };
+        case WebSocketActionTypes.CLOSE_POPUP:
+            return { ...state, isPopupOpen: false };
         // ... add more cases depending on actions
         default:
             return state;
     }
 };
 
-export default webSockerReducer;
+export default webSocketReducer;
