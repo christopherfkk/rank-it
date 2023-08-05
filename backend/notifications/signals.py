4dace3fe27329446dac85a2d11148a1a4039c689
Christopher Fok <christopherfkk@gmail.com>
2023-08-05 17:44:17 +0900

    Revert to wsgi

--- backend/notifications/signals.py
+++ backend/notifications/signals.py
@@ -7,15 +7,15 @@ from .models import Notification
 from .serializers import NotificationSerializer
 
 
-@receiver(post_save, sender=Notification)
-def push_latest_notification(sender, instance, **kwargs):
-    queryset = Notification.objects.filter(notifier=instance.notifier, status="Unread")
-    serializer = NotificationSerializer(queryset, many=True)
-    data = serializer.data
-
-    channel_layer = get_channel_layer()
-    message = {
-        'type': 'latest_notification',
-        'notification': data,
-    }
-    async_to_sync(channel_layer.group_send)(f'realtime-notification-{instance.notifier.id}', message)
+# @receiver(post_save, sender=Notification)
+# def push_latest_notification(sender, instance, **kwargs):
+#     queryset = Notification.objects.filter(notifier=instance.notifier, status="Unread")
+#     serializer = NotificationSerializer(queryset, many=True)
+#     data = serializer.data
+#
+#     channel_layer = get_channel_layer()
+#     message = {
+#         'type': 'latest_notification',
+#         'notification': data,
+#     }
+#     async_to_sync(channel_layer.group_send)(f'realtime-notification-{instance.notifier.id}', message)
