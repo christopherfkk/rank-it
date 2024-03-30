001294646567c0336f3b21a228f698397d3607a3
Christopher Fok <66243575+christopherfkk@users.noreply.github.com>
2024-03-31 05:25:09 +0900

    Update

--- frontend/utils/apiConfig.ts
+++ frontend/utils/apiConfig.ts
@@ -2,8 +2,8 @@ const apiConfig  = {
     BASE_URL: process.env.EXPO_PUBLIC_BASE_URL,
     WEB_SOCKET_BASE_URL: process.env.EXPO_PUBLIC_WEB_SOCKET_BASE_URL,
     google: {
-        clientId: "68084720196-u4a0f5ah112920qpn8bq342ov8h6i82v.apps.googleusercontent.com",
-        iosClientId: "68084720196-a2l8185469oj62i2jl8g9jmqi65lfmu5.apps.googleusercontent.com",
-        androidClientId: "68084720196-0p37fkv7oi57clhrvlboa02iad4mep19.apps.googleusercontent.com",
+        clientId: "",
+        iosClientId: "",
+        androidClientId: "",
     },}
 export default apiConfig;
